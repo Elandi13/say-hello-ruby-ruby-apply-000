ec2652e964ba1aadd5bf64cbdfa2e212233669a8
Elvis Landi <elandi13@yahoo.com>
2020-09-26 01:53:25 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -6,3 +6,5 @@ describe "say_hello" do 	describe "say_hello" do
   it 'accepts an argument of a name and prints out Hello with that Name' do	 
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
     say_hello("Kent Beck")
+  end
+end
